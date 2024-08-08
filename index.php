05253640d5b8a0cc88ee44cd43f575ee36e58a48
Hafiz Numan <84693709+Engr-Numan@users.noreply.github.com>
2024-08-07 22:28:21 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 
-print "i'm numan\n";
+print "i'm numan DevOps Engineer\n";
 print "adding new feature\n <br />;"
+print "adding new more more  feature\n <br />;"
+print "adding less less less feature\n <br />;"
+
 
 ?>
